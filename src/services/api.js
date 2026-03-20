9723d179bf26126428a0e22340878151305b8840
ajay <ajaykushwaha626162@gmail.com>
2026-03-20 19:07:47 +0530

    Fixed backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://vyomedge-backend.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
